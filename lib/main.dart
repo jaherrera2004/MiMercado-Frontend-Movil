5dcd46b740bd68b6a03de51b785c1abf362a02f0
Jose Polo <josedanielpolo04@hotmail.com>
2025-09-27 21:24:21 -0500

    Configuracion con firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mi_mercado/MiMercadoApp.dart';
+import 'package:mi_mercado/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(MiMercadoApp());
 }
 
